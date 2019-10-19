42058a2b3357d0881d5721c73aac64ee74d3d0fc
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-19 01:10:43 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,26 @@
 class Artist
-  attr_accessor :name
+  attr_accessor :title, :author
 
-  def initialize(name)
-    @name = name
+  @@all = []
+
+  def initialize(title)
+    @title = title
+    save
   end
 
+  def save
+    @@all << self
+  end
 
+  def self.all
+    @@all
+  end
+
+  def author_name
+    if self.author
+      self.author.name
+    else
+      nil
+    end
+  end
 end
