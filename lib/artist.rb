5b6fec93b9ff015198b52fa81e9fd58b7475ff41
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-19 01:34:28 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,9 +2,9 @@ class Artist
   attr_accessor :name
 
   @@song_count = 0
+
   def initialize(name)
     @name = name
-    @@song_count += 1
   end
 
   def add_song(song)
@@ -18,6 +18,7 @@ class Artist
   def add_song_by_name(name)
     song = Song.new(name)
     add_song(song)
+    @@song_count += 1
   end
 
   def self.song_count
