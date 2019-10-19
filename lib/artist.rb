10420ac0b8ed23bd711281e60675fdf09ff682a3
Daniel Alves de Carvalho <danielcarvalhovr@gmail.com>
2019-10-19 01:17:29 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,8 +1,16 @@
 class Artist
-  attr_accessor :name, :songs
+  attr_accessor :name
 
   def initialize(name)
     @name = name
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
   end
 
 
